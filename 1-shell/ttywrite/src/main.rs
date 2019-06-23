979d4f0281e9cb64a1ff965f95beb1631aabf443
zhouzyue <zhouzyue@gmail.com>
2019-06-23 22:33:30 +0800

    ttywrite

--- 1-shell/ttywrite/src/main.rs
+++ 1-shell/ttywrite/src/main.rs
@@ -49,14 +49,36 @@ struct Opt {
 
 fn main() {
     use std::fs::File;
-    use std::io::{self, BufReader, BufRead};
+    use std::io::{self, BufReader};
 
     let opt = Opt::from_args();
     let mut serial = serial::open(&opt.tty_path).expect("path points to invalid TTY");
 
     // FIXME: Implement the `ttywrite` utility.
-    let stdin = io::stdin();
-    let mut buf = String::new();
-    stdin.read_line(&mut buf);
-//    serial.write()
+    let mut settings = serial.read_settings().expect("");
+    settings.set_char_size(opt.char_width);
+    settings.set_stop_bits(opt.stop_bits);
+    settings.set_flow_control(opt.flow_control);
+    serial.write_settings(&settings).expect("invalid settings");
+    serial.set_timeout(Duration::from_secs(opt.timeout)).expect("invalid timeout");
+
+    let mut reader: Box<dyn io::Read> = match opt.input {
+        Some(ref path) => {
+            let file = File::open(path).expect("file not exist!");
+            Box::new(BufReader::new(file))
+        },
+        None => Box::new(io::stdin()),
+    };
+
+    if opt.raw {
+        let sent = io::copy(reader.as_mut(), &mut serial).unwrap();
+        println!("wrote {} bytes to {:#?}", sent, opt.tty_path);
+    } else {
+        Xmodem::transmit_with_progress(reader.as_mut(), &mut serial, progress_fn)
+            .unwrap();
+    }
+}
+
+fn progress_fn(progress: Progress) {
+    println!("Progress: {:?}", progress);
 }
